b8d0fb172c4f62c8ead34a97b9534ab9820f1a84
Hany Khamis <hani.khamis@gmail.com>
2023-02-27 00:51:56 +0300

    Introduce Children Props

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Header() {
+export default function Header(props) {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -144,6 +144,7 @@ export default function Header() {
               ))}
             </div>
           </Disclosure.Panel>
+          {props.children}
         </>
       )}
     </Disclosure>
---
+++ src/pages/Employees.js
@@ -0,0 +1,76 @@
+import "../index.css";
+import Employee from "../components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "../components/AddEmployee";
+
+export default function Employees() {
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Helga Richard",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
+    },
+    {
+      id: 2,
+      name: "Linda Anderson",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg",
+    },
+    {
+      id: 3,
+      name: "Harold Smith",
+      role: "Tester",
+      img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg",
+    },
+    {
+      id: 4,
+      name: "Harry Gate",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/10057618/pexels-photo-10057618.jpeg",
+    },
+  ]);
+
+  const addEmployee = (name, role, img) => {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  };
+  const updateEmployee = (id, name, role) => {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: name, role: role };
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  };
+  return (
+    <div className="App bg-blue-50 min-h-screen">
+      <div className="App-body flex flex-wrap justify-center my-2">
+        {employees.map((employee) => {
+          return (
+            <Employee
+              key={uuidv4()}
+              id={employee.id}
+              name={employee.name}
+              role={employee.role}
+              img={employee.img}
+              employee={employee}
+              updateEmployee={updateEmployee}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <AddEmployee addEmployee={addEmployee} />
+      </div>
+    </div>
+  );
+}
