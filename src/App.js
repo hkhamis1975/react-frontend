a4a4431938d71fbdf9e03b2f03af4e1832a44747
Hany Khamis <hani.khamis@gmail.com>
2023-02-26 11:37:38 +0300

    using props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="App-body">
+        <Employee name="Hany" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Employee.js
@@ -0,0 +1,9 @@
+function Employee(props) {
+  return (
+    <div>
+      <h3>Hello Employee {props.name}</h3>
+    </div>
+  );
+}
+
+export default Employee;
