4f0e6f27fbcb822f3e7a8b9115e055126d47acf1
Hany Khamis <hani.khamis@gmail.com>
2023-02-27 01:59:15 +0300

    Create Navbar navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./index.css";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customers from "./pages/Customers";
+import Dictionary from "./pages/Dictionary";
 
 function App() {
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Employees />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/dictionary" element={<Dictionary />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Dictionary.js
@@ -0,0 +1,7 @@
+export default function Dictionary() {
+  return (
+    <>
+      <h1> Hello Dictionary</h1>
+    </>
+  );
+}
