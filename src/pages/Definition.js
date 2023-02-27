e8e6f461cb2688a6cff49befd94b0e4ac1b9960e
Hany Khamis <hani.khamis@gmail.com>
2023-02-27 13:51:05 +0300

    handle 404

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link, useLocation } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import NotFound from "../components/NotFound";
 
 export default function Definition() {
   const [word, setWord] = useState();
+  const [notFound, setNotFound] = useState(false);
   let { search } = useParams();
 
   //   const {
@@ -13,14 +15,27 @@ export default function Definition() {
   //   } = useFetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search);
 
   useEffect(() => {
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search).then(
-      (res) =>
-        res.json().then((data) => {
-          setWord(data[0].meanings);
-          // console.log(data[0].meanings);
-        })
-    );
+    fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
+      .then((res) => {
+        if (res.status === 404) {
+          setNotFound(true);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setWord(data[0].meanings);
+        // console.log(data[0].meanings);
+      });
   }, [search]);
+
+  if (notFound) {
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionary">Search again</Link>
+      </>
+    );
+  }
 
   return (
     <>
